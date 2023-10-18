7245ed997b0b4a08d16b4367aee4b6628ee5fdb3
Aashika Raj <126551058+aashikaraj@users.noreply.github.com>
2023-10-18 11:13:11 +0530

    Add files via upload

--- cuisine.php
+++ cuisine.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Wandering Tales</title>
+    <title>Kolkata's Food</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -12,7 +12,7 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="food_cuisine.css" />
-    <script src="cuisine.js"></script>
+    <script src="js/cuisine.js"></script>
 </head>
 
 <body>
@@ -39,22 +39,24 @@
     <section class="showcase-area" id="showcase">
         <div class="showcase-container">
             <h1 class="main-title" id="home">Eat Tasty Food !</h1>
-            <p>Eat Kolkata's famous food.</p>
+            <p class="w3-large">Eat Kolkata's famous food.
+            </p>
             <a href="#food-menu" class="btn btn-primary">Menu</a>
         </div>
     </section>
     <section id="about">
         <div class="about-wrapper container">
                 <h2>Kolkata famous food lasts for years</h2>
-                <b>Delight Your Taste Buds with Authentic Dishes
-                What better way to truly experience a new place than through its food? Our website not only highlights famous places but also introduces you to authentic dishes that you may not find in mainstream restaurants. We've curated a collection of local delicacies, from street food to fine dining experiences. Each dish is accompanied by a description, ingredients, and even the price range, ensuring you have all the information you need to make the most delicious choices.</b>
-            </div>
+                <b>Delight  Taste Buds with Authentic Dishes
+                What better way to truly experience a new place than through its food? Our website not only highlights famous places but also introduces you to authentic dishes that you may not find in mainstream restaurants. We've curated a collection of local delicacies, from street food to fine dining experiences. Each dish is accompanied by a description, ingredients, and even the price range, ensuring you have all the information you need to make the most delicious choices.
+        </div>
+    </section>    
+
             <div class="about-img">
                 <img src="Mishti-Doi-.jpg" alt="food"/>
-                <h1 style="border: 2px solid rgb(250, 120, 97);"></h1>
+                <h9><style="border: 2px solid rgb(250, 120, 97);"></h9>
             </div>
-        </div>
-    </section>
+        
     <section id="food">
         <div class="section2">
            
@@ -63,13 +65,13 @@
 		</div>
 		</div>
         <div class="container">
-
-           <div class="box1"><img src="allen-kitchen.jpg"width="650" height="400"></div>
+           <div class="box1"><img src="allen.jpg"width="650" height="400"></div>
            <div class="box2">
            <h1>Allen Kitchen</h1>
                 This 132-year-old restaurant is a proud landmark of the city’s exquisite cuisine serving a variety of finger food. This restaurant operates only for 5 hours between 4 pm to 9 pm but is always packed no matter when you decide to go. It has been a favourite for generations for its reliable cooking and lip-smacking dishes.
-                   <br> Where: <br>40/1, Jatindra Mohan Avenue, Shobha Bazaar.
-            </div>
+                   <br> Where: <br>40/1, Jatindra Mohan Avenue, Shobha Bazaar.<br>
+                   <br><a href="https://www.google.com/search?sxsrf=AB5stBhtUhrzdCn5l9ANf1Tbha88R08anA:1688389963898&q=allen+kitchen&tbm=isch&sa=X&ved=2ahUKEwjY9qjgzvL_AhXQTmwGHaQNAKQQ0pQJegQICRAB&biw=1536&bih=763&dpr=1.25"  class="btn">More</a></div>
+
         </div>
         <div class="container">
 
@@ -77,18 +79,22 @@
            <h1>Golbari</h1>
            Kolkata is incomplete without its North Kolkata charm, which brings me to my next place. For generations on end, Golbari and Kosha Mangsho have been synonymous. What’s so exquisite about this place is that even after 94 years of running, it has never changed its -. This places once known as the New Punjabi restaurant is now colloquially been renamed to Golbari for its spherical facade ( ‘gol’ meaning circle in Bengali). What most people do not know about this place is that Golbari famous for serving Bengali delicacies like Kosha Mangsho was started by a Punjabi and not a Bengali. If you are in Kolkata, your trip won’t be worthwhile until you try the soft mutton cooked in the dark velvety texture of gravy otherwise popularly known as Kosha Mangsho. And mind you, they have no branches!
                 <br> Where: <br>
-                New Punjabi Hotel, 211, A.P.C. Road, Shyam Bazaar</div>
+                New Punjabi Hotel, 211, A.P.C. Road, Shyam Bazaar<br>
+                Must Have: Kosha Mangso.<br>
+                <br><a href="https://restaurant-guru.in/Gol-Bari-Kolkata"  class="btn">More</a>
+         </div>
            <div class="box2"><img src="golbari.jpg" width="650" height="400"></div>
 
         </div>
         <div class="container">
 
-           <div class="box1"><img src="fluryaposs.jpg" width="650" height="400"></div>
+           <div class="box1"><img src="flurys.jpg" width="650" height="400"></div>
            <div class="box2">
            <h1>Flury’s</h1>
            A must stop for all those with a sweet tooth. This place was a famous one amongst the English, as a popular tearoom. Established in 1927, Flury’s primed in serving confectionary items to all its customers. Other than the regular cakes and pastries, the English breakfast is a favoured choice for all its regulars. If you want a taste of the city’s one of the finest baked cakes, pastries and other such Swiss and English delicacies, then this is the place not to be missed at all!
                   <br>Where:<br>
-                  18, Park Street, Park Street Area.</div>
+                  18, Park Street, Park Street Area.<br>
+                  <br><a href="https://www.mouthshut.com/product-reviews/Flurys-Park-Street-Kolkata-reviews-925696234"  class="btn">Menu</a></div>
 
         </div>
         <div class="container">
@@ -97,17 +103,21 @@
            <h1>Dilkusha Cabin</h1>
            Serving for more than a century, located in the center of College Street, this was once a renowned place for their rendition of the legendary dish called Kabiraji Cutlet. Dilkusha literally means ‘ heart-pleasing’, and is still running serving its legendary Kabiraji and cutlet. It bears testimony to the culture and the tradition of the older generation. This 108-year-old cabin had six other cabins spread in the city, the one in College Street still survives. 
                     <br> Where: <br>
-                    88, Mahatma Gandhi Rd, College Square</div>
-           <div class="box2"><img src="dilkhusha-cabin.jpg" width="650" height="400"></div>
+                    88, Mahatma Gandhi Rd, College Square<br>
+                    <br><a href="https://www.tripadvisor.in/Restaurant_Review-g304558-d7779736-Reviews-Dilkhush_Cabin-Kolkata_Calcutta_Kolkata_District_West_Bengal.html"  class="btn">Menu</a></div>
+
+           <div class="box2"><img src="dilkhusha-c.jpg" width="650" height="400"></div>
 
         </div>
         <div class="container">
 
-           <div class="box1"><img src="anadi-cabin.jpg" width="650" height="400"></div>
+           <div class="box1"><img src="anadi.jpg" width="650" height="400"></div>
            <div class="box2"><h1>Anadi Cabin</h1>
+
                     Another illustrious cabin in the city, it still receives a number of visitors for its history of serving for generations till now. It is one of those timeless eat outs with customers of all age groups. Famous for its keema filled Mughlai paratha served with a side of potato curry and onion salad, a taste of its specialties has kept its customers wanting for more for over 60 years now.
                    <br> Where:<br>
-                    9, Jawaharlal Nehru Road, New Market Area </div>
+                    9, Jawaharlal Nehru Road, New Market Area <br>
+                    <br><a href="https://yummraj.com/2014/10/19/anadi-cabin-on-sn-bannerjee-road-jl-nehru-road-crossing-9a-jawaharlal-nehru-road-esplanade-kolkata/"  class="btn">Menu</a></div>
 
         </div>
         <div class="container">
@@ -117,7 +127,8 @@
                
                     For all those who know Kolkata, even a little bit will realise that Park Street will be a frequent on this list with a large number of legendary restaurants its boasts of, one of them being Peter Cat. Located in Park Street, Peter Cat popularised for its unique Chelo Kebab has been in the business since 1975. Their Chelo Kebab Platter which is a combination of buttered rice with poached eggs and grilled kebabs and veggies is so unique that it is no surprise that you will often see customers queuing up for the privilege of tasting the specials, especially during the festivals and the New Year’s.    
                      Where: <br>
-                    18A, Park Street, Park Street Area</div>
+                    18A, Park Street, Park Street Area<br>
+                    <br><a href="https://www.google.com/search?sxsrf=AB5stBi3kX8-yfq_oYU4mSUI2YRZnDajmA:1688390715346&q=peter+cat&tbm=isch&sa=X&ved=2ahUKEwjI39HG0fL_AhWQf94KHQN6CSoQ0pQJegQIDBAB&biw=1536&bih=706&dpr=1.25#imgrc=xTvHn4gossE5TM"  class="btn">Menu</a></div>
            <div class="box2"><img src="petercimg.jpg" width="650" height="400"></div>
 
         </div>
@@ -129,7 +140,8 @@
                     Serving Biryanis from a numerous centers, the first Aminia was opened in Dharmatala near New Market. What very few people would know is that this place came into existence on the day of independence and saw itself into popularity because of the young and novice footballers of Bengal who would turn up for a plate of biryani from their favourite Biryani place after a good game.
                 
                    <br> Where: <br>
-                    6A, S.N. Banerjee Road, New Market Area</div>
+                    6A, S.N. Banerjee Road, New Market Area<br>
+                    <br><a href="https://www.aminia.co.in/aminia-golpark"  class="btn">Menu</a></div>
 
         </div>
         <div class="container">
@@ -138,13 +150,14 @@
             <h1>Mocambo</h1>
                     Mocambo is a regular destination for families who have been visiting the place as a course of its lineage. Known for its courteous staff and the treatment to its customers, Mocambo is on the list for all the right reasons. Pioneering in dishes like Fish A La Diana, Chicken Ala Kiev, Angels on Horseback, this place has been a favourite for foreigners and expatriates in the city. For those who do not know this, Mocambo is such a legendary restaurant that it had been scripted into the Bollywood movie, Kahani.
                    <br>Where: <br>
-                    25B Park Street, Park Street Area  </div>
+                    25B Park Street, Park Street Area <br>
+                    <br><a href="https://www.google.com/search?gs_ssp=eJzj4tVP1zc0TDYsMCzMzrA0YLRSNagwTjQwMjc3MDUzSDVNTUs2tTKoMEs2NDI2NTIztDRLNE42TvNiz81PTsxNygcAF9IRYg&q=mocambo&rlz=1C1CHZL_enIN933IN933&oq=mocam&aqs=chrome.1.69i57j46i175i199i433i512j0i512j46i131i433i512j0i512l5.5087j0j7&sourceid=chrome&ie=UTF-8"  class="btn">Menu</a></div>
            <div class="box2"><img src="mocambo.jpg" width="650" height="400"></div>
 
         </div>
         <div class="container">
 
-           <div class="box1"><img src="mutton chaap royal.jpg" width="650" height="400"></div>
+           <div class="box1"><img src="r.jpg" width="650" height="400"></div>
            <div class="box2"> 
             <h1>Royal Indian Hotel</h1>
                    A Biryani place, which arguably the first ones to bring Biryani to this city. It competes with its multiple adversaries (other biryani chains in the city) with the difference of the potato being absent from any of the biryanis they serve. According to their history, they have since their establishment served Lucknowi Biryani. Even though it does not contain a Bengali’s favourite element of biryani, this place is full most of the times.
@@ -152,7 +165,8 @@
                  147, Rabindra Sarani, Bara Bazar
 
                     <br> Must Have: <br>
-                  Chicken/ Mutton Biryani, Mutton Chaap</div>
+                  Chicken/ Mutton Biryani, Mutton Chaap<br>
+                  <br><a href="https://magicpin.in/Kolkata/Burrabazar/Restaurant/Royal-Indian-Hotel/store/19c22a/menu/"  class="btn">Menu</a></div>
 
         </div>
         <div class="container">
@@ -163,8 +177,8 @@
              Nobin Chandra Das (1845–1925) was a Bengali confectioner, entrepreneur, businessman and Bengali cultural icon of the second half of 19th century and early 20th century.
                 Where:
                 <br>77, Jatindra Mohan Ave, Sovabazar, Shobhabazar, Kolkata, <br>
-           </div>
-           <div class="box2"><img src="rosogolla nobin chandra.jpg" width="650" height="400"></div>
+                <br><a href="https://www.google.com/search?q=nobin+chandra+das+shop&rlz=1C1CHZL_enIN933IN933&oq=&aqs=chrome.0.35i39i362l2j46i39i175i199i362j35i39i362l5.25372569j0j7&sourceid=chrome&ie=UTF-8"  class="btn">Menu</a></div>
+           <div class="box2"><img src="roso.jpg" width="650" height="400"></div>
 
         </div>
         
